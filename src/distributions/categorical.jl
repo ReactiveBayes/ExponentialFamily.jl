0ca8e850efa2540a27ed852633d271f029d53d1b
ismail <i.senoz@tue.nl>
2023-03-02 17:09:03 +0100

    added first set of EF extensions and tests

--- src/distributions/categorical.jl
+++ src/distributions/categorical.jl
@@ -1,4 +1,5 @@
 export Categorical
+export CategoricalNaturalParameters, lognormalizer, naturalparams
 
 import Distributions: Categorical, probs
 
@@ -20,3 +21,81 @@ probvec(dist::Categorical) = probs(dist)
 function compute_logscale(new_dist::Categorical, left_dist::Categorical, right_dist::Categorical)
     return log(dot(probvec(left_dist), probvec(right_dist)))
 end
+
+
+struct CategoricalNaturalParameters{T <: Real} <: NaturalParameters
+    logprobabilities::Array{T}
+end
+
+function CategoricalNaturalParameters(logprobabilities::AbstractVector{<:Real})
+    T = promote_type(eltype(logprobabilities))
+    return CategoricalNaturalParameters(convert(AbstractArray{T}, logprobabilities))
+end
+
+function CategoricalNaturalParameters(logproabilities::AbstractVector{<:Integer})
+    return CategoricalNaturalParameters(float.(logproabilities))
+end
+
+function CategoricalNaturalParameters(logprobabilities::AbstractVector{T}) where T
+    return CategoricalNaturalParameters{T}(logprobabilities)
+end
+
+
+Base.convert(::Type{CategoricalNaturalParameters}, logprobabilities::AbstractVector) =
+    convert(CategoricalNaturalParameters{promote_type(eltype(logprobabilities))}, logprobabilities)
+
+Base.convert(::Type{CategoricalNaturalParameters{T}}, logprobabilities::AbstractVector) where {T} =
+    CategoricalNaturalParameters(convert(AbstractVector{T}, logprobabilities))
+
+Base.convert(::Type{CategoricalNaturalParameters}, vector::AbstractVector) = convert(CategoricalNaturalParameters{eltype(vector)}, vector)
+
+Base.convert(::Type{CategoricalNaturalParameters{T}}, vector::AbstractVector) where {T} = CategoricalNaturalParameters(convert(AbstractVector{T}, vector))
+
+function Base.:(==)(left::CategoricalNaturalParameters, right::CategoricalNaturalParameters)
+    return left.logprobabilities == right.logprobabilities 
+end
+
+as_naturalparams(::Type{T}, args...) where {T <: CategoricalNaturalParameters} = convert(CategoricalNaturalParameters, args...)
+
+
+function Base.vec(p::CategoricalNaturalParameters)
+    return p
+end
+
+# Standard parameters to natural parameters
+function naturalparams(dist::Categorical)
+    logprobabilities = log.(probvec(dist))
+    return CategoricalNaturalParameters(logprobabilities)
+end
+
+
+function convert(::Type{Distribution}, η::CategoricalNaturalParameters)
+    return Categorical(softmax(η.logprobabilities))
+end
+
+
+function Base.:+(left::CategoricalNaturalParameters, right::CategoricalNaturalParameters)
+    return CategoricalNaturalParameters(left.logprobabilities .+ right.logprobabilities)
+end
+
+
+function Base.:-(left::CategoricalNaturalParameters, right::CategoricalNaturalParameters)
+    return CategoricalNaturalParameters(left.logprobabilities .- right.logprobabilities)
+end
+
+
+function lognormalizer(::CategoricalNaturalParameters)
+    return 0.0
+end
+
+
+function Distributions.logpdf(η::CategoricalNaturalParameters, x)
+    return Distributions.logpdf(convert(Categorical,η),x)
+end
+
+isproper(params::CategoricalNaturalParameters) = true
+
+
+
+
+
