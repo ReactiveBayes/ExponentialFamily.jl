06c65265d96c87855713fa24cc843b4bb5281cdf
ismail <i.senoz@tue.nl>
2023-03-13 17:06:49 +0100

    ran formatter

--- src/distributions/von_mises.jl
+++ src/distributions/von_mises.jl
@@ -5,7 +5,6 @@ import SpecialFunctions: besselj0
 
 vague(::Type{<:VonMises}) = VonMises(0.0, tiny)
 
-
 prod_analytical_rule(::Type{<:VonMises}, ::Type{<:VonMises}) = ProdAnalyticalRuleAvailable()
 
 function Base.prod(::ProdAnalytical, left::VonMises, right::VonMises)
@@ -15,27 +14,27 @@ function Base.prod(::ProdAnalytical, left::VonMises, right::VonMises)
     return Base.convert(Distribution, naturalparams)
 end
 
-isproper(params::NaturalParameters{VonMises}) = true 
+isproper(params::NaturalParameters{VonMises}) = true
 
 function Base.convert(::Type{NaturalParameters}, dist::VonMises)
     μ, κ = params(dist)
-    NaturalParameters(VonMises, [κ*cos(μ), κ*sin(μ)])
+    NaturalParameters(VonMises, [κ * cos(μ), κ * sin(μ)])
 end
 
 function Base.convert(::Type{Distribution}, η::NaturalParameters{VonMises})
     params = get_params(η)
     κcosμ  = first(params)
 
-    κ = sqrt(params'*params)
-    μ = acos(κcosμ/κ)
+    κ = sqrt(params' * params)
+    μ = acos(κcosμ / κ)
     return VonMises(μ, κ)
 end
 
 check_valid_natural(::Type{<:VonMises}, v) = length(v) === 2
 
-function lognormalizer(params::NaturalParameters{VonMises}) 
+function lognormalizer(params::NaturalParameters{VonMises})
     η = get_params(params)
-    κ = sqrt(η'*η)
+    κ = sqrt(η' * η)
     return log(besselj0(κ))
 end
-basemeasure(T::Union{<:NaturalParameters{VonMises}, <:VonMises}, x) = 1/2pi
+basemeasure(T::Union{<:NaturalParameters{VonMises}, <:VonMises}, x) = 1 / 2pi
