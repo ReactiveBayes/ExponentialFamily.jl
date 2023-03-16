ba0c01093a830a38516616f6fa6c120ac49d5aef
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2023-03-16 19:07:56 +0100

    feat: add Weibull

--- src/distributions/weibull.jl
+++ src/distributions/weibull.jl
@@ -1,4 +1,6 @@
-import Distributions: Weibull
+export Weibull
+
+import Distributions: Weibull, Distribution
 
 check_valid_natural(::Type{<:Weibull}, params) = length(params) === 1
 check_valid_conditioner(::Type{<:Weibull}, conditioner) = isreal(conditioner) && conditioner > 0
@@ -8,3 +10,18 @@ function isproper(params::NaturalParameters{Weibull})
     return first(η) < 0
 end
 
+basemeasure(np::NaturalParameters{Weibull}, x) = x^(get_conditioner(np) - 1)
+basemeasure(dist::Weibull, x) = x^(shape(dist) - 1)
+
+Base.convert(::Type{NaturalParameters}, dist::Pareto) =
+    NaturalParameters(Weibull, [-(1 / scale(dist))^(shape(dist))], shape(dist))
+
+function Base.convert(::Type{Distribution}, np::NaturalParameters{Weibull})
+    k = get_condioner(np)
+    η = first(get_params(np))
+    return Weibull(k, (-1 / η)^(1 / k))
+end
+
+function lognormalizer(np::NaturalParameters{Weibull})
+    return -log(first(get_params(np))) - log(get_condioner(np))
+end
