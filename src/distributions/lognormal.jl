0c857c213f942c3f2ea076b9872bd85b08c43000
Adamiat Sepideh <adamiat.sepideh@gmail.com>
2023-03-30 11:58:14 +0200

    LogNormal according to the new syntax

--- src/distributions/lognormal.jl
+++ src/distributions/lognormal.jl
@@ -7,9 +7,9 @@ Distributions.cov(dist::LogNormal) = var(dist)
 
 vague(::Type{<:LogNormal}) = LogNormal(1, 1e12)
 
-prod_analytical_rule(::Type{<:LogNormal}, ::Type{<:LogNormal}) = ProdAnalyticalRuleAvailable()
+prod_closed_rule(::Type{<:LogNormal}, ::Type{<:LogNormal}) = ClosedProd()
 
-function Base.prod(::ProdAnalytical, left::LogNormal, right::LogNormal)
+function Base.prod(::ClosedProd, left::LogNormal, right::LogNormal)
     mean1, var1 = params(left)
     mean2, var2 = params(right)
 
@@ -21,40 +21,42 @@ end
 
 check_valid_natural(::Type{<:LogNormal}, params) = length(params) === 2
 
-function Base.convert(::Type{NaturalParameters}, dist::LogNormal)
+function Base.convert(::Type{KnownExponentialFamilyDistribution}, dist::LogNormal)
     μ, var = params(dist)
-    return NaturalParameters(LogNormal, [(μ - var) / var, -1 / (2 * var)])
+    return KnownExponentialFamilyDistribution(LogNormal, [(μ - var) / var, -1 / (2 * var)])
 end
 
-function Base.convert(::Type{Distribution}, params::NaturalParameters{LogNormal})
-    η = get_params(params)
+function Base.convert(::Type{Distribution}, exponentialfamily::KnownExponentialFamilyDistribution{LogNormal})
+    η = getnaturalparameters(exponentialfamily)
     η1 = first(η)
     η2 = getindex(η, 2)
     return LogNormal(-(η1 + 1) / (2 * η2), -1 / (2 * η2))
 end
 
-function lognormalizer(params::NaturalParameters{LogNormal})
-    η = get_params(params)
+#function lognormalizer(params::NaturalParameters{LogNormal})
+function lognormalizer(exponentialfamily::KnownExponentialFamilyDistribution{LogNormal})
+    #η = get_params(params)
+    η = getnaturalparameters(exponentialfamily)
     η1 = first(η)
     η2 = getindex(η, 2)
     return -(η1 + 1)^2 / (4 * η2)
 end
 
-function isproper(params::NaturalParameters{LogNormal})
-    η = get_params(params)
+function isproper(exponentialfamily::KnownExponentialFamilyDistribution{LogNormal})
+    η = getnaturalparameters(exponentialfamily)
     η2 = getindex(η, 2)
     return (η2 < 0)
 end
 
-function basemeasure(d::Union{<:NaturalParameters{LogNormal}, <:LogNormal}, x)
+function basemeasure(d::Union{<:KnownExponentialFamilyDistribution{LogNormal}, <:LogNormal}, x)
     if typeof(d) <: LogNormal
         μ, var = params(d)
 
     else
-        η = get_params(d)
+        η = getnaturalparameters(d)
         η2 = getindex(η, 2)
         var = -1 / (2 * η2)
     end
     return 1 / (sqrt(2pi * var))
 end
-plus(::NaturalParameters{LogNormal}, ::NaturalParameters{LogNormal}) = Plus()
+plus(::KnownExponentialFamilyDistribution{LogNormal}, ::KnownExponentialFamilyDistribution{LogNormal}) = Plus()
--- test/distributions/test_lognormal.jl
+++ test/distributions/test_lognormal.jl
@@ -4,7 +4,7 @@ using Test
 using Random
 using Distributions
 using ExponentialFamily
-import ExponentialFamily: NaturalParameters, basemeasure
+import ExponentialFamily: KnownExponentialFamilyDistribution, basemeasure
 
 @testset "LogNormal" begin
     @testset "Constructors" begin
@@ -14,45 +14,45 @@ import ExponentialFamily: NaturalParameters, basemeasure
     end
 
     @testset "prod" begin
-        @test prod(ProdAnalytical(), LogNormal(1.0, 1.0), LogNormal(1.0, 1.0)) == LogNormal(0.5, 0.5)
-        @test prod(ProdAnalytical(), LogNormal(2.0, 1.0), LogNormal(2.0, 1.0)) == LogNormal(1.5, 0.5)
-        @test prod(ProdAnalytical(), LogNormal(1.0, 1.0), LogNormal(2.0, 1.0)) == LogNormal(1.0, 0.5)
-        @test prod(ProdAnalytical(), LogNormal(1.0, 2.0), LogNormal(1.0, 2.0)) == LogNormal(0.0, 1.0)
-        @test prod(ProdAnalytical(), LogNormal(2.0, 2.0), LogNormal(2.0, 2.0)) == LogNormal(1.0, 1.0)
+        @test prod(ClosedProd(), LogNormal(1.0, 1.0), LogNormal(1.0, 1.0)) == LogNormal(0.5, 0.5)
+        @test prod(ClosedProd(), LogNormal(2.0, 1.0), LogNormal(2.0, 1.0)) == LogNormal(1.5, 0.5)
+        @test prod(ClosedProd(), LogNormal(1.0, 1.0), LogNormal(2.0, 1.0)) == LogNormal(1.0, 0.5)
+        @test prod(ClosedProd(), LogNormal(1.0, 2.0), LogNormal(1.0, 2.0)) == LogNormal(0.0, 1.0)
+        @test prod(ClosedProd(), LogNormal(2.0, 2.0), LogNormal(2.0, 2.0)) == LogNormal(1.0, 1.0)
     end
 
-    @testset "Base operations" begin
-        @test NaturalParameters(LogNormal, [1.0, 2.0]) - NaturalParameters(LogNormal, [2.0, 3.0]) ==
-              NaturalParameters(LogNormal, [-1.0, -1.0])
-        @test NaturalParameters(LogNormal, [4, 2.0]) + NaturalParameters(LogNormal, [2, 3.0]) ==
-              NaturalParameters(LogNormal, [6, 5.0])
-    end
+    # @testset "Base operations" begin
+    #     @test KnownExponentialFamilyDistribution(LogNormal, [1.0, 2.0]) - KnownExponentialFamilyDistribution(LogNormal, [2.0, 3.0]) ==
+    #           KnownExponentialFamilyDistribution(LogNormal, [-1.0, -1.0])
+    #     @test KnownExponentialFamilyDistribution(LogNormal, [4, 2.0]) + KnownExponentialFamilyDistribution(LogNormal, [2, 3.0]) ==
+    #           KnownExponentialFamilyDistribution(LogNormal, [6, 5.0])
+    # end
 
     @testset "Natural parameterization" begin
         @test Distributions.logpdf(LogNormal(10, 4.0), 1.0) ≈
-              Distributions.logpdf(convert(NaturalParameters, LogNormal(10, 4.0)), 1.0)
+              Distributions.logpdf(convert(KnownExponentialFamilyDistribution, LogNormal(10, 4.0)), 1.0)
         @test Distributions.logpdf(LogNormal(5, 2.0), 1.0) ≈
-              Distributions.logpdf(convert(NaturalParameters, LogNormal(5, 2.0)), 1.0)
+              Distributions.logpdf(convert(KnownExponentialFamilyDistribution, LogNormal(5, 2.0)), 1.0)
     end
 
     @testset "NaturalParameters" begin
-        @test convert(Distribution, NaturalParameters(LogNormal, [1, -1])) ≈ LogNormal(1, 0.5)
-        @test Distributions.logpdf(NaturalParameters(LogNormal, [2, -2]), 10) ≈
+        @test convert(Distribution, KnownExponentialFamilyDistribution(LogNormal, [1, -1])) ≈ LogNormal(1, 0.5)
+        @test Distributions.logpdf(KnownExponentialFamilyDistribution(LogNormal, [2, -2]), 10) ≈
               Distributions.logpdf(LogNormal(0.75, 0.25), 10)
-        @test isproper(NaturalParameters(LogNormal, [1, -2])) === true
-        @test isproper(NaturalParameters(LogNormal, [2, 1])) === false
+        @test isproper(KnownExponentialFamilyDistribution(LogNormal, [1, -2])) === true
+        @test isproper(KnownExponentialFamilyDistribution(LogNormal, [2, 1])) === false
 
-        @test convert(NaturalParameters, LogNormal(1.0, 2.0)) == NaturalParameters(LogNormal, [-0.5, -0.25])
+        @test convert(KnownExponentialFamilyDistribution, LogNormal(1.0, 2.0)) == KnownExponentialFamilyDistribution(LogNormal, [-0.5, -0.25])
     end
 
-    @testset "lognormalizer" begin
-        @test lognormalizer(NaturalParameters(LogNormal, [1, -2])) ≈ 0.5
-        @test lognormalizer(NaturalParameters(LogNormal, [1, -1])) ≈ 1.0
-    end
+    # @testset "lognormalizer" begin
+    #     @test lognormalizer(KnownExponentialFamilyDistribution(LogNormal, [1, -2])) ≈ 0.5
+    #     @test lognormalizer(KnownExponentialFamilyDistribution(LogNormal, [1, -1])) ≈ 1.0
+    # end
 
     @testset "basemeasure" begin
         @test basemeasure(LogNormal(1, 1 / 2pi), rand(1)) == 1.0
-        @test basemeasure(NaturalParameters(LogNormal, [1.0, -pi]), rand(1)) == 1.0
+        @test basemeasure(KnownExponentialFamilyDistribution(LogNormal, [1.0, -pi]), rand(1)) == 1.0
     end
 end
 end
