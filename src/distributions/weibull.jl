e9d38a32db4819b65849bb04d4368806456e473e
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2023-03-31 19:12:06 +0200

    fix: rewrite according latest changes

--- src/distributions/weibull.jl
+++ src/distributions/weibull.jl
@@ -5,16 +5,16 @@ import Distributions: Weibull, Distribution
 check_valid_natural(::Type{<:Weibull}, params) = length(params) === 1
 check_valid_conditioner(::Type{<:Weibull}, conditioner) = isreal(conditioner) && conditioner > 0
 
-function isproper(params::NaturalParameters{Weibull})
-    η = get_params(params)
+function isproper(exponentialfamily::KnownExponentialFamilyDistribution{Weibull})
+    η = get_params(exponentialfamily)
     return first(η) < 0
 end
 
-basemeasure(np::NaturalParameters{Weibull}, x) = x^(get_conditioner(np) - 1)
 basemeasure(dist::Weibull, x) = x^(shape(dist) - 1)
+basemeasure(weibull::KnownExponentialFamilyDistribution{Weibull}, x) = x^(get_conditioner(weibull) - 1)
 
-Base.convert(::Type{NaturalParameters}, dist::Pareto) =
-    NaturalParameters(Weibull, [-(1 / scale(dist))^(shape(dist))], shape(dist))
+Base.convert(::Type{KnownExponentialFamilyDistribution}, dist::Weibull) =
+    KnownExponentialFamilyDistribution(Weibull, [-(1 / scale(dist))^(shape(dist))], shape(dist))
 
 function Base.convert(::Type{Distribution}, np::NaturalParameters{Weibull})
     k = get_condioner(np)
@@ -22,6 +22,6 @@ function Base.convert(::Type{Distribution}, np::NaturalParameters{Weibull})
     return Weibull(k, (-1 / η)^(1 / k))
 end
 
-function lognormalizer(np::NaturalParameters{Weibull})
-    return -log(first(get_params(np))) - log(get_condioner(np))
+function logpartition(exponentialfamily::KnownExponentialFamilyDistribution{Weibull})
+    return -log(first(get_params(exponentialfamily))) - log(get_condioner(exponentialfamily))
 end
