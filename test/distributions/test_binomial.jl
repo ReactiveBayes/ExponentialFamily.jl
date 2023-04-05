26f48559eb4195c76188740cce101000c7bb5adc
Albert Podusenko <albert.podusenko@gmail.com>
2023-04-05 20:45:08 +0200

    Make format

--- test/distributions/test_binomial.jl
+++ test/distributions/test_binomial.jl
@@ -28,31 +28,31 @@ import ExponentialFamily: KnownExponentialFamilyDistribution, getnaturalparamete
         left = Binomial(10, 0.5)
         right = Binomial(15, 0.3)
         prod_dist = prod(ClosedProd(), left, right)
-        
+
         sample_points = collect(1:5)
         for x in sample_points
             @test prod_dist.basemeasure(x) == (binomial(10, x) * binomial(15, x))
             @test prod_dist.sufficientstatistics(x) == x
         end
-    
+
         sample_points = [-5, -2, 0, 2, 5]
         for η in sample_points
             @test prod_dist.logpartition(η) == log(pFq([-10, -15], [1], exp(η)))
         end
-    
+
         @test prod_dist.naturalparameters == [logit(0.5) + logit(0.3)]
         @test prod_dist.support == support(left)
-    
+
         sample_points = collect(1:5)
         for x in sample_points
-            hist_sum(x) = prod_dist.basemeasure(x) * exp(
-                prod_dist.sufficientstatistics(x) * prod_dist.naturalparameters[1] -
-                prod_dist.logpartition(prod_dist.naturalparameters[1])
-            )
-            @test sum(hist_sum(x) for x in 0:20) ≈ 1.0 atol=1e-5
+            hist_sum(x) =
+                prod_dist.basemeasure(x) * exp(
+                    prod_dist.sufficientstatistics(x) * prod_dist.naturalparameters[1] -
+                    prod_dist.logpartition(prod_dist.naturalparameters[1])
+                )
+            @test sum(hist_sum(x) for x in 0:20) ≈ 1.0 atol = 1e-5
         end
     end
-    
 
     @testset "naturalparameter related Binomial" begin
         d1 = Binomial(5, 1 / 3)
