c6329202480476b583091172bdc453c2b382401e
Albert Podusenko <albert.podusenko@gmail.com>
2023-04-05 16:51:42 +0200

    Make format

--- test/distributions/test_chi_squared.jl
+++ test/distributions/test_chi_squared.jl
@@ -6,7 +6,8 @@ using Random
 using Distributions
 
 import SpecialFunctions: logfactorial, loggamma
-import ExponentialFamily: xtlog, KnownExponentialFamilyDistribution, getnaturalparameters, basemeasure, ExponentialFamilyDistribution
+import ExponentialFamily:
+    xtlog, KnownExponentialFamilyDistribution, getnaturalparameters, basemeasure, ExponentialFamilyDistribution
 
 @testset "Chisq" begin
     @testset "ChisqKnownExponentialFamilyDistribution" begin
@@ -27,20 +28,18 @@ import ExponentialFamily: xtlog, KnownExponentialFamilyDistribution, getnaturalp
             left = Chisq(i + 1)
             right = Chisq(i)
             prod_dist = prod(ClosedProd(), left, right)
-    
+
             η_left = first(getnaturalparameters(convert(KnownExponentialFamilyDistribution, left)))
             η_right = first(getnaturalparameters(convert(KnownExponentialFamilyDistribution, right)))
             naturalparameters = [η_left + η_right]
-    
+
             @test prod_dist.naturalparameters == naturalparameters
             @test prod_dist.basemeasure(i) ≈ exp(-i)
             @test prod_dist.sufficientstatistics(i) ≈ log(i)
             @test prod_dist.logpartition(η_left + η_right) ≈ loggamma(η_left + η_right + 1)
             @test prod_dist.support === support(left)
-
         end
     end
-    
 
     @testset "Natural parameterization tests" begin
         @test Distributions.logpdf(Chisq(10), 1.0) ≈
