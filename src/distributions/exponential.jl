8b9a413ea1219d7148cbeb97505af299cd8e8970
Albert Podusenko <albert.podusenko@gmail.com>
2023-05-10 16:24:02 +0200

    Add fisher infromation for exponential distribution

--- src/distributions/exponential.jl
+++ src/distributions/exponential.jl
@@ -38,3 +38,8 @@ end
 isproper(exponentialfamily::KnownExponentialFamilyDistribution{Exponential}) =
     (first(getnaturalparameters(exponentialfamily)) <= zero(Float64))
 basemeasure(::Union{<:KnownExponentialFamilyDistribution{Exponential}, <:Exponential}, x) = 1.0
+
+fisher_information(exponentialfamily::KnownExponentialFamilyDistribution{Exponential}) =
+    inv(first(getnaturalparameters(exponentialfamily))^2)
+
+fisher_information(dist::Exponential) = inv(dist.θ^2)
--- test/distributions/test_exponential.jl
+++ test/distributions/test_exponential.jl
@@ -4,8 +4,10 @@ using Test
 using ExponentialFamily
 using Distributions
 using Random
+using ForwardDiff
 
-import ExponentialFamily: mirrorlog, KnownExponentialFamilyDistribution, getnaturalparameters, basemeasure
+import ExponentialFamily:
+    mirrorlog, KnownExponentialFamilyDistribution, getnaturalparameters, basemeasure, fisher_information
 
 @testset "Exponential" begin
 
@@ -83,6 +85,23 @@ import ExponentialFamily: mirrorlog, KnownExponentialFamilyDistribution, getnatu
         @test convert(KnownExponentialFamilyDistribution, Exponential(1e12)) ==
               KnownExponentialFamilyDistribution(Exponential, [-1e-12])
         @test basemeasure(Exponential(5), rand()) == 1.0
+    end
+
+    @testset "fisher information" begin
+        θ = 5.0
+        dist = Exponential(θ)
+        ef = convert(KnownExponentialFamilyDistribution, dist)
+        η = getnaturalparameters(ef)
+
+        @test fisher_information(dist) ≈ 1 / θ^2 atol = 1e-8
+
+        samples = rand(Exponential(θ), 10000)
+        hessian = (x) -> -ForwardDiff.hessian((params) -> mean(logpdf.(Exponential(params[1]), samples)), x)
+        fisher_information(Exponential(θ)) ≈ first(hessian([θ]))
+
+        f_logpartion = (η) -> logpartition(KnownExponentialFamilyDistribution(Exponential, η))
+        autograd_inforamation = (η) -> ForwardDiff.hessian(f_logpartion, η)
+        @test fisher_information(ef) ≈ first(autograd_inforamation(η))
     end
 end
 
