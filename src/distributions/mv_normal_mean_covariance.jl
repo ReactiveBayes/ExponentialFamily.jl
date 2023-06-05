5696f28e9ed2c8412de3b7b9c2c81f183c1c129c
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2023-06-05 13:33:13 +0200

    fix: appropriate mean cov

--- src/distributions/mv_normal_mean_covariance.jl
+++ src/distributions/mv_normal_mean_covariance.jl
@@ -115,19 +115,17 @@ function fisherinformation(dist::MvNormalMeanCovariance{T}) where {T}
 
     _, P = weightedmean_invcov(dist)
 
-    hessian[(dim + 1):(dim^2+dim), (dim + 1):(dim^2+dim)] = kron(P, 1/2*P)
- 
-    # for i in (dim + 1):(dim^2+dim)
-    #     for j in (dim + 1):(dim^2+dim)
-    #         k, l, m, n = tensordoubleindex(i-dim, j-dim, dim)
-    #         if (k == l) && (m == n) && (k == m)
-    #             hessian[i, j] = 0.5 * P[k, k]^2
-    #         elseif (k == l || m != n) || (k != l || m == n)
-    #             hessian[i, j] = 0.5*P[k, m] * P[l, n]
-    #         else
-    #             hessian[i, j] = P[k, m] * P[l, n] + P[l, m] * P[k, n]
-    #         end
-    #     end
-    # end
+    for i in (dim + 1):(dim^2+dim)
+        for j in (dim + 1):(dim^2+dim)
+            k, l, m, n = tensordoubleindex(i-dim, j-dim, dim)
+            if (k == l) && (m == n) && (k == m)
+                hessian[i, j] = 0.5 * P[k, k]^2
+            elseif (k == l || m != n) || (k != l || m == n)
+                hessian[i, j] = P[k, m] * P[l, n]
+            else
+                hessian[i, j] = P[k, m] * P[l, n] + P[l, m] * P[k, n]
+            end
+        end
+    end
     return hessian
 end
