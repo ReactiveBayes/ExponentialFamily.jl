7d4f571e6ab68a727bf0b43c0bf8b1b3f66c53b9
ismail <i.senoz@tue.nl>
2023-06-12 22:02:17 +0300

    modify geometric distribution

--- src/distributions/geometric.jl
+++ src/distributions/geometric.jl
@@ -12,7 +12,7 @@ Base.prod(::ClosedProd, left::Geometric, right::Geometric) =
     Geometric(succprob(left) + succprob(right) - succprob(left) * succprob(right))
 
 Base.convert(::Type{KnownExponentialFamilyDistribution}, dist::Geometric) =
-    KnownExponentialFamilyDistribution(Geometric, [log(one(Float64) - succprob(dist))])
+    KnownExponentialFamilyDistribution(Geometric, log(one(Float64) - succprob(dist)))
 
 Base.convert(::Type{Distribution}, η::KnownExponentialFamilyDistribution{Geometric}) =
     Geometric(one(Float64) - exp(first(getnaturalparameters(η))))
@@ -23,14 +23,17 @@ logpartition(η::KnownExponentialFamilyDistribution{Geometric}) =
 check_valid_natural(::Type{<:Geometric}, params) = length(params) == 1
 
 function isproper(exponentialfamily::KnownExponentialFamilyDistribution{Geometric})
-    η = first(getnaturalparameters(exponentialfamily))
+    η = getnaturalparameters(exponentialfamily)
     return (η <= zero(η)) && (η >= log(convert(typeof(η), tiny)))
 end
 
-basemeasure(::Union{<:KnownExponentialFamilyDistribution{Geometric}, <:Geometric}, x) = 1.0
-
+function basemeasure(::Union{<:KnownExponentialFamilyDistribution{Geometric}, <:Geometric}, x) 
+    @assert typeof(x) <: Integer "basemeasure should be evaluated at integer values for Geometric distribution"
+    @assert 0 <= x "basemeasure should be evaluated at values greater than 0 "
+    return 1.0
+end 
 function fisherinformation(exponentialfamily::KnownExponentialFamilyDistribution{Geometric})
-    η = first(getnaturalparameters(exponentialfamily))
+    η = getnaturalparameters(exponentialfamily)
     exp(η) / (one(Float64) - exp(η))^2
 end
 
@@ -39,4 +42,10 @@ function fisherinformation(dist::Geometric)
     one(Float64) / (p * (one(Float64) - p)) + one(Float64) / p^2
 end
 
-sufficientstatistics(::Union{<:KnownExponentialFamilyDistribution{Geometric}, <:Geometric}, x::Int64) = x-1 
+function sufficientstatistics(::Union{<:KnownExponentialFamilyDistribution{Geometric}, <:Geometric}, x) 
+    @assert typeof(x) <: Integer "basemeasure should be evaluated at integer values for Geometric distribution"
+    @assert 0 <= x "basemeasure should be evaluated at values greater than 0 "
+    
+    return x
+end
+    
