b01f7bc347603d70c275d0e79dd4f364a51fdf50
ismail <i.senoz@tue.nl>
2023-07-25 13:10:32 +0300

    change rayleigh

--- src/distributions/rayleigh.jl
+++ src/distributions/rayleigh.jl
@@ -2,6 +2,7 @@ export Rayleigh
 
 import Distributions: Rayleigh, params
 using DomainSets
+using StaticArrays
 
 vague(::Type{<:Rayleigh}) = Rayleigh(Float64(huge))
 
@@ -16,8 +17,8 @@ function Base.prod(
     η2 = getnaturalparameters(right)
     naturalparameters = η1 + η2
     basemeasure = (x) -> 4 * x^2 / sqrt(pi)
-    sufficientstatistics = (x) -> x^2
-    logpartition = (η) -> log(η^(-3 / 2))
+    sufficientstatistics = (x) -> SA[x^2]
+    logpartition = (η) -> (-3 / 2)log(η)
     support = DomainSets.HalfLine()
 
     return ExponentialFamilyDistribution(
@@ -36,34 +37,35 @@ function Base.prod(::ClosedProd, left::T, right::T) where {T <: Rayleigh}
     return prod(ClosedProd(), ef_left, ef_right)
 end
 
-function isproper(ef::KnownExponentialFamilyDistribution{Rayleigh})
+pack_naturalparameters(dist::Rayleigh) = [MINUSHALF / first(params(dist))^2]
+function unpack_naturalparameters(ef::KnownExponentialFamilyDistribution{<:Rayleigh})
     η = getnaturalparameters(ef)
-    return (η < 0)
+    @inbounds η1 = η[1]
+    return η1
 end
 
-function Base.convert(::Type{KnownExponentialFamilyDistribution}, dist::Rayleigh)
-    σ = first(params(dist))
-    KnownExponentialFamilyDistribution(Rayleigh, -1 / (2 * σ^2))
-end
+isproper(ef::KnownExponentialFamilyDistribution{Rayleigh}) = unpack_naturalparameters(ef) < 0
 
+Base.convert(::Type{KnownExponentialFamilyDistribution}, dist::Rayleigh) = KnownExponentialFamilyDistribution(Rayleigh, pack_naturalparameters(dist))
+    
 function Base.convert(::Type{Distribution}, ef::KnownExponentialFamilyDistribution{Rayleigh})
-    η = getnaturalparameters(ef)
+    η = unpack_naturalparameters(ef)
     return Rayleigh(sqrt(-1 / (2η)))
 end
 
 check_valid_natural(::Type{<:Rayleigh}, v) = length(v) === 1
 
-logpartition(ef::KnownExponentialFamilyDistribution{Rayleigh}) = -log(-2 * getnaturalparameters(ef))
+logpartition(ef::KnownExponentialFamilyDistribution{Rayleigh}) = -log(-2 * unpack_naturalparameters(ef))
 
-fisherinformation(dist::Rayleigh) = 4 / scale(dist)^2
+fisherinformation(dist::Rayleigh) = [4 / scale(dist)^2]
 
-fisherinformation(ef::KnownExponentialFamilyDistribution{Rayleigh}) = inv(getnaturalparameters(ef)^2)
+fisherinformation(ef::KnownExponentialFamilyDistribution{Rayleigh}) = [inv(unpack_naturalparameters(ef)^2)]
 
 support(::KnownExponentialFamilyDistribution{Rayleigh}) = ClosedInterval{Real}(0, Inf)
 
 function sufficientstatistics(union::Union{<:KnownExponentialFamilyDistribution{Rayleigh}, <:Rayleigh}, x::Real)
     @assert insupport(union, x) "Rayleigh sufficient statistics should be evaluated at values greater than 0"
-    return x^2
+    return SA[x^2]
 end
 
 function basemeasure(union::Union{<:KnownExponentialFamilyDistribution{Rayleigh}, <:Rayleigh}, x::Real)
