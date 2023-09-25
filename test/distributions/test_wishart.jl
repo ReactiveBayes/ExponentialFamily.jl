52b1879c4cca12907c3e6f6fb62999a6c3739401
ismail <i.senoz@tue.nl>
2023-09-25 11:13:58 +0200

    wisharts tests are passing

--- test/distributions/test_wishart.jl
+++ test/distributions/test_wishart.jl
@@ -20,62 +20,62 @@ import StatsFuns: logmvgamma
     # Wishart comes from Distributions.jl and most of the things should be covered there
     # Here we test some extra ExponentialFamily.jl specific functionality
 
-    # @testset "mean(::logdet)" begin
-    #     @test mean(logdet, Wishart(3, [1.0 0.0; 0.0 1.0])) ≈ 0.845568670196936
-    #     @test mean(
-    #         logdet,
-    #         Wishart(
-    #             5,
-    #             [
-    #                 1.4659658963311604 1.111775094889733 0.8741034114800605
-    #                 1.111775094889733 0.8746971141492232 0.6545661366809246
-    #                 0.8741034114800605 0.6545661366809246 0.5498917856395482
-    #             ]
-    #         )
-    #     ) ≈ -3.4633310802040693
-    # end
+    @testset "mean(::logdet)" begin
+        @test mean(logdet, Wishart(3, [1.0 0.0; 0.0 1.0])) ≈ 0.845568670196936
+        @test mean(
+            logdet,
+            Wishart(
+                5,
+                [
+                    1.4659658963311604 1.111775094889733 0.8741034114800605
+                    1.111775094889733 0.8746971141492232 0.6545661366809246
+                    0.8741034114800605 0.6545661366809246 0.5498917856395482
+                ]
+            )
+        ) ≈ -3.4633310802040693
+    end
 
-    # @testset "mean(::cholinv)" begin
-    #     L    = rand(2, 2)
-    #     S    = L * L' + diageye(2)
-    #     invS = cholinv(S)
-    #     @test mean(inv, Wishart(5, S)) ≈ mean(InverseWishart(5, invS))
-    #     @test mean(cholinv, Wishart(5, S)) ≈ mean(InverseWishart(5, invS))
-    # end
+    @testset "mean(::cholinv)" begin
+        L    = rand(2, 2)
+        S    = L * L' + diageye(2)
+        invS = cholinv(S)
+        @test mean(inv, Wishart(5, S)) ≈ mean(InverseWishart(5, invS))
+        @test mean(cholinv, Wishart(5, S)) ≈ mean(InverseWishart(5, invS))
+    end
 
-    # @testset "vague" begin
-    #     @test_throws MethodError vague(Wishart)
+    @testset "vague" begin
+        @test_throws MethodError vague(Wishart)
 
-    #     d = vague(Wishart, 3)
+        d = vague(Wishart, 3)
 
-    #     @test typeof(d) <: Wishart
-    #     @test mean(d) == Matrix(Diagonal(3 * 1e12 * ones(3)))
-    # end
+        @test typeof(d) <: Wishart
+        @test mean(d) == Matrix(Diagonal(3 * 1e12 * ones(3)))
+    end
 
-    # @testset "rand" begin
-    #     for d in (2, 3, 4, 5)
-    #         v = rand() + d
-    #         L = rand(d, d)
-    #         S = L' * L + d * diageye(d)
-    #         invS = cholinv(S)
-    #         cS = copy(S)
-    #         cinvS = copy(invS)
-    #         container1 = [zeros(d, d) for _ in 1:100]
-    #         container2 = [zeros(d, d) for _ in 1:100]
+    @testset "rand" begin
+        for d in (2, 3, 4, 5)
+            v = rand() + d
+            L = rand(d, d)
+            S = L' * L + d * diageye(d)
+            invS = cholinv(S)
+            cS = copy(S)
+            cinvS = copy(invS)
+            container1 = [zeros(d, d) for _ in 1:100]
+            container2 = [zeros(d, d) for _ in 1:100]
 
-    #         # Check inplace versions
-    #         @test rand!(StableRNG(321), Wishart(v, S), container1) ≈
-    #               rand!(StableRNG(321), WishartFast(v, invS), container2)
+            # Check inplace versions
+            @test rand!(StableRNG(321), Wishart(v, S), container1) ≈
+                  rand!(StableRNG(321), WishartFast(v, invS), container2)
 
-    #         # Check that matrices are not corrupted
-    #         @test all(S .=== cS)
-    #         @test all(invS .=== cinvS)
+            # Check that matrices are not corrupted
+            @test all(S .=== cS)
+            @test all(invS .=== cinvS)
 
-    #         # Check non-inplace versions
-    #         @test rand(StableRNG(321), Wishart(v, S), length(container1)) ≈
-    #               rand(StableRNG(321), WishartFast(v, invS), length(container2))
-    #     end
-    # end
+            # Check non-inplace versions
+            @test rand(StableRNG(321), Wishart(v, S), length(container1)) ≈
+                  rand(StableRNG(321), WishartFast(v, invS), length(container2))
+        end
+    end
 
     @testset "ExponentialFamilyDistribution{WishartFast}" begin
         @testset for dim in (3), invS in rand(Wishart(10,diageye(dim)),2)
@@ -93,63 +93,37 @@ import StatsFuns: logmvgamma
             end
         end
 
-    # @testset "prod" begin
-    #     inv_v1 = cholinv([9.0 -3.4; -3.4 11.0])
-    #     inv_v2 = cholinv([10.2 -3.3; -3.3 5.0])
-    #     inv_v3 = cholinv([8.1 -2.7; -2.7 9.0])
+    @testset "prod" begin
+        inv_v1 = cholinv([9.0 -3.4; -3.4 11.0])
+        inv_v2 = cholinv([10.2 -3.3; -3.3 5.0])
+        inv_v3 = cholinv([8.1 -2.7; -2.7 9.0])
 
-    #     @test prod(ClosedProd(), WishartFast(3, inv_v1), WishartFast(3, inv_v2)) ≈
-    #           WishartFast(
-    #         3,
-    #         cholinv([4.776325721474591 -1.6199382410125422; -1.6199382410125422 3.3487476649765537])
-    #     )
-    #     @test prod(ClosedProd(), WishartFast(4, inv_v1), WishartFast(4, inv_v3)) ≈
-    #           WishartFast(
-    #         5,
-    #         cholinv([4.261143738311623 -1.5064864332819319; -1.5064864332819319 4.949867121624725])
-    #     )
-    #     @test prod(ClosedProd(), WishartFast(5, inv_v2), WishartFast(4, inv_v3)) ≈
-    #           WishartFast(6, cholinv([4.51459128065395 -1.4750681198910067; -1.4750681198910067 3.129155313351499]))
-    # end
+        @test prod(PreserveTypeProd(Distribution), WishartFast(3, inv_v1), WishartFast(3, inv_v2)) ≈
+              WishartFast(
+            3,
+            cholinv([4.776325721474591 -1.6199382410125422; -1.6199382410125422 3.3487476649765537])
+        )
+        @test prod(PreserveTypeProd(Distribution), WishartFast(4, inv_v1), WishartFast(4, inv_v3)) ≈
+              WishartFast(
+            5,
+            cholinv([4.261143738311623 -1.5064864332819319; -1.5064864332819319 4.949867121624725])
+        )
+        @test prod(PreserveTypeProd(Distribution), WishartFast(5, inv_v2), WishartFast(4, inv_v3)) ≈
+              WishartFast(6, cholinv([4.51459128065395 -1.4750681198910067; -1.4750681198910067 3.129155313351499]))
+    end
 
-    # @testset "ndims" begin
-    #     @test ndims(vague(Wishart, 3)) === 3
-    #     @test ndims(vague(Wishart, 4)) === 4
-    #     @test ndims(vague(Wishart, 5)) === 5
-    # end
+    @testset "prod with ExponentialFamilyDistribution{Wishart}" begin
+        for Sleft in rand(Wishart(10,diageye(2)),2), Sright in rand(Wishart(10,diageye(2)),2), νright in (6,7), νleft in (4,5)
+            let left = WishartFast(νleft, Sleft), right = WishartFast(νright, Sright)
+                @test test_generic_simple_exponentialfamily_product(
+                    left,
+                    right,
+                    strategies = (PreserveTypeProd(ExponentialFamilyDistribution{WishartFast}), GenericProd())
+                )
+            end
+        end
+    end
 
-  
-
-    # @testset "fisher information" begin
-    #     rng = StableRNG(42)
-    #     for df in 3:6
-    #         L = randn(rng, df, df)
-    #         A = L * L' + 1e-8 * diageye(df)
-    #         dist = Wishart(df, A)
-    #         distfast = WishartFast(df, cholinv(A))
-    #         ef = convert(ExponentialFamilyDistribution, dist)
-    #         η_vec = getnaturalparameters(ef)
-    #         fef = fisherinformation(ef)
-    #         fdist = fisherinformation(dist)
-    #         fdistfast = fisherinformation(distfast)
-    #         ## We do not test the analytic solution agains autograd because autograd hessian return values that are permuted and
-    #         ## causes fisherinformation to be non-positive definite.
-    #         J = ForwardDiff.jacobian(transformation, η_vec)
-    #         @test fef ≈ J' * fdist * J rtol = 1e-8
-    #         @test fdist ≈ fdistfast rtol = 1e-8
-    #         f_logpartition = (η_vec) -> logpartition(ef, η_vec)
-    #         autograd_information = (η_vec) -> ForwardDiff.hessian(f_logpartition, η_vec)
-    #         ag_fi = autograd_information(η_vec)
-
-    #         svdfisheref = svd(fef)
-    #         svdautograd = svd(ag_fi)
-    #         ## Julia returns very small complex values which causes problems. Therefore we take the real parts. 
-    #         eigenfisheref = real.(eigvals(fef))
-    #         @test all(x -> x > 0, eigenfisheref)
-    #         @test svdfisheref.S ≈ svdautograd.S
-    #     end
-    # end
-    
 
 end
 
--- test/distributions/test_wishart_inverse.jl
+++ test/distributions/test_wishart_inverse.jl
@@ -45,244 +45,161 @@ include("../testutils.jl")
         end
     end
 
-    # @testset "statistics" begin
-    #     rng = StableRNG(42)
-    #     # ν > dim(d) + 1
-    #     for ν in 4:10
-    #         L = randn(rng, ν - 2, ν - 2)
-    #         S = L * L'
-    #         d = InverseWishartFast(ν, S)
 
-    #         @test mean(d) == mean(InverseWishart(params(d)...))
-    #         @test mode(d) == mode(InverseWishart(params(d)...))
-    #     end
+    @testset "statistics" begin
+        rng = StableRNG(42)
+        # ν > dim(d) + 1
+        for ν in 4:10
+            L = randn(rng, ν - 2, ν - 2)
+            S = L * L'
+            d = InverseWishartFast(ν, S)
 
-    #     # ν > dim(d) + 3
-    #     for ν in 5:10
-    #         L = randn(rng, ν - 4, ν - 4)
-    #         S = L * L'
-    #         d = InverseWishartFast(ν, S)
+            @test mean(d) == mean(InverseWishart(params(d)...))
+            @test mode(d) == mode(InverseWishart(params(d)...))
+        end
 
-    #         @test cov(d) == cov(InverseWishart(params(d)...))
-    #         @test var(d) == var(InverseWishart(params(d)...))
-    #     end
-    # end
+        # ν > dim(d) + 3
+        for ν in 5:10
+            L = randn(rng, ν - 4, ν - 4)
+            S = L * L'
+            d = InverseWishartFast(ν, S)
 
-    # @testset "vague" begin
-    #     @test_throws MethodError vague(InverseWishartFast)
+            @test cov(d) == cov(InverseWishart(params(d)...))
+            @test var(d) == var(InverseWishart(params(d)...))
+        end
+    end
 
-    #     dims = 3
-    #     d1 = vague(InverseWishart, dims)
+    @testset "vague" begin
+        @test_throws MethodError vague(InverseWishartFast)
 
-    #     @test typeof(d1) <: InverseWishart
-    #     ν1, S1 = params(d1)
-    #     @test ν1 == dims + 2
-    #     @test S1 == tiny .* diageye(dims)
+        dims = 3
+        d1 = vague(InverseWishart, dims)
 
-    #     @test mean(d1) == S1
+        @test typeof(d1) <: InverseWishart
+        ν1, S1 = params(d1)
+        @test ν1 == dims + 2
+        @test S1 == tiny .* diageye(dims)
 
-    #     dims = 4
-    #     d2 = vague(InverseWishart, dims)
+        @test mean(d1) == S1
 
-    #     @test typeof(d2) <: InverseWishart
-    #     ν2, S2 = params(d2)
-    #     @test ν2 == dims + 2
-    #     @test S2 == tiny .* diageye(dims)
+        dims = 4
+        d2 = vague(InverseWishart, dims)
 
-    #     @test mean(d2) == S2
-    # end
+        @test typeof(d2) <: InverseWishart
+        ν2, S2 = params(d2)
+        @test ν2 == dims + 2
+        @test S2 == tiny .* diageye(dims)
 
-    # @testset "entropy" begin
-    #     @test entropy(
-    #         InverseWishartFast(
-    #             2.0,
-    #             [2.2658069783329573 -0.47934965873423374; -0.47934965873423374 1.4313564100863712]
-    #         )
-    #     ) ≈ 10.111427477184794
-    #     @test entropy(InverseWishartFast(5.0, diageye(4))) ≈ 8.939145914882221
-    # end
+        @test mean(d2) == S2
+    end
 
-    # @testset "convert" begin
-    #     rng = StableRNG(42)
-    #     for ν in 2:10
-    #         L = randn(rng, ν, ν)
-    #         S = L * L'
-    #         d = InverseWishartFast(ν, S)
-    #         @test convert(InverseWishart, d) == InverseWishart(ν, S)
-    #     end
-    # end
+    @testset "entropy" begin
+        @test entropy(
+            InverseWishartFast(
+                2.0,
+                [2.2658069783329573 -0.47934965873423374; -0.47934965873423374 1.4313564100863712]
+            )
+        ) ≈ 10.111427477184794
+        @test entropy(InverseWishartFast(5.0, diageye(4))) ≈ 8.939145914882221
+    end
 
-    # @testset "mean(::typeof(logdet))" begin
-    #     rng = StableRNG(123)
-    #     ν, S = 2.0, [2.2658069783329573 -0.47934965873423374; -0.47934965873423374 1.4313564100863712]
-    #     samples = rand(rng, InverseWishart(ν, S), Int(1e6))
-    #     @test isapprox(mean(logdet, InverseWishartFast(ν, S)), mean(logdet.(samples)), atol = 1e-2)
+    @testset "convert" begin
+        rng = StableRNG(42)
+        for ν in 2:10
+            L = randn(rng, ν, ν)
+            S = L * L'
+            d = InverseWishartFast(ν, S)
+            @test convert(InverseWishart, d) == InverseWishart(ν, S)
+        end
+    end
 
-    #     ν, S = 4.0, diageye(3)
-    #     samples = rand(rng, InverseWishart(ν, S), Int(1e6))
-    #     @test isapprox(mean(logdet, InverseWishartFast(ν, S)), mean(logdet.(samples)), atol = 1e-2)
-    # end
+    @testset "mean(::typeof(logdet))" begin
+        rng = StableRNG(123)
+        ν, S = 2.0, [2.2658069783329573 -0.47934965873423374; -0.47934965873423374 1.4313564100863712]
+        samples = rand(rng, InverseWishart(ν, S), Int(1e6))
+        @test isapprox(mean(logdet, InverseWishartFast(ν, S)), mean(logdet.(samples)), atol = 1e-2)
 
-    # @testset "mean(::typeof(inv))" begin
-    #     rng = StableRNG(321)
-    #     ν, S = 2.0, [2.2658069783329573 -0.47934965873423374; -0.47934965873423374 1.4313564100863712]
-    #     samples = rand(rng, InverseWishart(ν, S), Int(1e6))
-    #     @test isapprox(mean(inv, InverseWishartFast(ν, S)), mean(inv.(samples)), atol = 1e-2)
-    #     @test isapprox(mean(cholinv, InverseWishartFast(ν, S)), mean(cholinv.(samples)), atol = 1e-2)
+        ν, S = 4.0, diageye(3)
+        samples = rand(rng, InverseWishart(ν, S), Int(1e6))
+        @test isapprox(mean(logdet, InverseWishartFast(ν, S)), mean(logdet.(samples)), atol = 1e-2)
+    end
 
-    #     ν, S = 4.0, diageye(3)
-    #     samples = rand(rng, InverseWishart(ν, S), Int(1e6))
-    #     @test isapprox(mean(inv, InverseWishartFast(ν, S)), mean(inv.(samples)), atol = 1e-2)
-    #     @test isapprox(mean(cholinv, InverseWishartFast(ν, S)), mean(cholinv.(samples)), atol = 1e-2)
-    # end
+    @testset "mean(::typeof(inv))" begin
+        rng = StableRNG(321)
+        ν, S = 2.0, [2.2658069783329573 -0.47934965873423374; -0.47934965873423374 1.4313564100863712]
+        samples = rand(rng, InverseWishart(ν, S), Int(1e6))
+        @test isapprox(mean(inv, InverseWishartFast(ν, S)), mean(inv.(samples)), atol = 1e-2)
+        @test isapprox(mean(cholinv, InverseWishartFast(ν, S)), mean(cholinv.(samples)), atol = 1e-2)
 
-    # @testset "prod" begin
-    #     d1 = InverseWishartFast(3.0, diageye(2))
-    #     d2 = InverseWishartFast(-3.0, [0.6423504672769315 0.9203141654948761; 0.9203141654948761 1.528137747462735])
+        ν, S = 4.0, diageye(3)
+        samples = rand(rng, InverseWishart(ν, S), Int(1e6))
+        @test isapprox(mean(inv, InverseWishartFast(ν, S)), mean(inv.(samples)), atol = 1e-2)
+        @test isapprox(mean(cholinv, InverseWishartFast(ν, S)), mean(cholinv.(samples)), atol = 1e-2)
+    end
 
-    #     @test prod(ClosedProd(), d1, d2) ≈
-    #           InverseWishartFast(3.0, [1.6423504672769313 0.9203141654948761; 0.9203141654948761 2.528137747462735])
+    @testset "prod" begin
+        d1 = InverseWishartFast(3.0, diageye(2))
+        d2 = InverseWishartFast(-3.0, [0.6423504672769315 0.9203141654948761; 0.9203141654948761 1.528137747462735])
 
-    #     d1 = InverseWishartFast(4.0, diageye(3))
-    #     d2 = InverseWishartFast(-2.0, diageye(3))
+        @test prod(PreserveTypeProd(Distribution), d1, d2) ≈
+              InverseWishartFast(3.0, [1.6423504672769313 0.9203141654948761; 0.9203141654948761 2.528137747462735])
 
-    #     @test prod(ClosedProd(), d1, d2) ≈ InverseWishartFast(6.0, 2 * diageye(3))
-    # end
+        d1 = InverseWishartFast(4.0, diageye(3))
+        d2 = InverseWishartFast(-2.0, diageye(3))
 
-    # @testset "rand" begin
-    #     for d in (2, 3, 4, 5)
-    #         v = rand() + d
-    #         L = rand(d, d)
-    #         S = L' * L + d * diageye(d)
-    #         cS = copy(S)
-    #         container1 = [zeros(d, d) for _ in 1:100]
-    #         container2 = [zeros(d, d) for _ in 1:100]
+        @test prod(PreserveTypeProd(Distribution), d1, d2) ≈ InverseWishartFast(6.0, 2 * diageye(3))
+    end
 
-    #         # Check in-place version
-    #         @test rand!(StableRNG(321), InverseWishart(v, S), container1) ≈
-    #               rand!(StableRNG(321), InverseWishartFast(v, S), container2)
+    @testset "rand" begin
+        for d in (2, 3, 4, 5)
+            v = rand() + d
+            L = rand(d, d)
+            S = L' * L + d * diageye(d)
+            cS = copy(S)
+            container1 = [zeros(d, d) for _ in 1:100]
+            container2 = [zeros(d, d) for _ in 1:100]
 
-    #         # Check that the matrix has not been corrupted
-    #         @test all(S .=== cS)
+            # Check in-place version
+            @test rand!(StableRNG(321), InverseWishart(v, S), container1) ≈
+                  rand!(StableRNG(321), InverseWishartFast(v, S), container2)
 
-    #         # Check non-inplace version
-    #         @test rand(StableRNG(321), InverseWishart(v, S), length(container1)) ≈
-    #               rand(StableRNG(321), InverseWishartFast(v, S), length(container2))
-    #     end
-    # end
+            # Check that the matrix has not been corrupted
+            @test all(S .=== cS)
 
-    # @testset "pdf!" begin
-    #     for d in (2, 3, 4, 5), n in (10, 20)
-    #         v = rand() + d
-    #         L = rand(d, d)
-    #         S = L' * L + d * diageye(d)
+            # Check non-inplace version
+            @test rand(StableRNG(321), InverseWishart(v, S), length(container1)) ≈
+                  rand(StableRNG(321), InverseWishartFast(v, S), length(container2))
+        end
+    end
 
-    #         samples = map(1:n) do _
-    #             L_sample = rand(d, d)
-    #             return L_sample' * L_sample + d * diageye(d)
-    #         end
+    @testset "pdf!" begin
+        for d in (2, 3, 4, 5), n in (10, 20)
+            v = rand() + d
+            L = rand(d, d)
+            S = L' * L + d * diageye(d)
 
-    #         result = zeros(n)
+            samples = map(1:n) do _
+                L_sample = rand(d, d)
+                return L_sample' * L_sample + d * diageye(d)
+            end
 
-    #         @test all(pdf(InverseWishart(v, S), samples) .≈ pdf!(result, InverseWishartFast(v, S), samples))
-    #     end
-    # end
+            result = zeros(n)
 
-    # @testset "InverseWishartExponentialFamilyDistribution" begin
-    #     @testset "Constructor" begin
-    #         for i in 1:10
-    #             @test convert(
-    #                 Distribution,
-    #                 ExponentialFamilyDistribution(InverseWishartFast, vcat(-3.0, vec([-i 0.0; 0.0 -i])))
-    #             ) ≈ InverseWishart(3.0, -2([-i 0.0; 0.0 -i]))
-    #         end
-    #     end
+            @test all(pdf(InverseWishart(v, S), samples) .≈ pdf!(result, InverseWishartFast(v, S), samples))
+        end
+    end
 
-    #     @testset "logpdf" begin
-    #         for i in 1:10
-    #             wishart_np = ExponentialFamilyDistribution(InverseWishartFast, vcat(-3.0, vec([-i 0.0; 0.0 -i])))
-    #             distribution = InverseWishart(3.0, -2([-i 0.0; 0.0 -i]))
-    #             @test logpdf(distribution, [1.0 0.0; 0.0 1.0]) ≈ logpdf(wishart_np, [1.0 0.0; 0.0 1.0])
-    #             @test logpdf(distribution, [1.0 0.2; 0.2 1.0]) ≈ logpdf(wishart_np, [1.0 0.2; 0.2 1.0])
-    #             @test logpdf(distribution, [1.0 -0.1; -0.1 3.0]) ≈ logpdf(wishart_np, [1.0 -0.1; -0.1 3.0])
-    #         end
-    #     end
-
-    #     @testset "logpartition" begin
-    #         @test logpartition(
-    #             ExponentialFamilyDistribution(InverseWishartFast, vcat(-3.0, vec([-1.0 0.0; 0.0 -1.0])))
-    #         ) ≈
-    #               logmvgamma(2, 1.5)
-    #     end
-
-    #     @testset "isproper" begin
-    #         for i in 1:10
-    #             @test isproper(
-    #                 ExponentialFamilyDistribution(InverseWishartFast, vcat(3.0, vec([-i 0.0; 0.0 -i])))
-    #             ) ===
-    #                   false
-    #             @test isproper(
-    #                 ExponentialFamilyDistribution(InverseWishartFast, vcat(3.0, vec([i 0.0; 0.0 -i])))
-    #             ) ===
-    #                   false
-    #             @test isproper(
-    #                 ExponentialFamilyDistribution(InverseWishartFast, vcat(-1.0, vec([-i 0.0; 0.0 -i])))
-    #             ) ===
-    #                   true
-    #         end
-    #     end
-
-    #     @testset "basemeasure" begin
-    #         for i in 1:10
-    #             L = rand(2, 2)
-    #             @test basemeasure(
-    #                 ExponentialFamilyDistribution(InverseWishartFast, vcat(3.0, vec([-i 0.0; 0.0 -i]))),
-    #                 L * L'
-    #             ) == 1.0
-    #         end
-    #     end
-
-    #     @testset "base operations" begin
-    #         for i in 1:10
-    #             np1 = ExponentialFamilyDistribution(InverseWishartFast, vcat(3.0, vec([i 0.0; 0.0 i])))
-    #             np2 = ExponentialFamilyDistribution(InverseWishartFast, vcat(3.0, vec([2i 0.0; 0.0 2i])))
-    #             @test prod(np1, np2) == ExponentialFamilyDistribution(
-    #                 InverseWishartFast,
-    #                 vcat(3.0, vec([i 0.0; 0.0 i])) + vcat(3.0, vec([2i 0.0; 0.0 2i]))
-    #             )
-    #         end
-    #     end
-
-    #     @testset "fisherinformation" begin
-    #         rng = StableRNG(42)
-    #         for d in 2:30
-    #             for df in d:30
-    #                 L = randn(rng, d, d)
-    #                 A = L * L' + 1e-8 * diageye(d)
-    #                 dist = InverseWishart(df, A)
-    #                 ef = convert(ExponentialFamilyDistribution, dist)
-    #                 η_vec = getnaturalparameters(ef)
-    #                 fef = fisherinformation(ef)
-    #                 fdist = fisherinformation(dist)
-
-    #                 J = ForwardDiff.jacobian(transformation, η_vec)
-    #                 @test fef ./ (J' * fdist * J) ≈ ones(size(fef))
-    #             end
-    #         end
-    #     end
-
-    #     @testset "ExponentialFamilyDistribution mean,cov" begin
-    #         rng = StableRNG(42)
-    #         for df in 2:20
-    #             L = randn(rng, df, df)
-    #             A = L * L' + 1e-8 * diageye(df)
-    #             dist = InverseWishart(df + 4, A)
-    #             ef = convert(ExponentialFamilyDistribution, dist)
-    #             @test mean(dist) ≈ mean(ef) rtol = 1e-8
-    #             @test cov(dist) ≈ cov(ef) rtol = 1e-8
-    #         end
-    #     end
-    # end
+    @testset "prod with ExponentialFamilyDistribution{InverseWishartFast}" begin
+        for Sleft in rand(InverseWishart(10,diageye(2)),2), Sright in rand(InverseWishart(10,diageye(2)),2), νright in (6,7), νleft in (4,5)
+            let left = InverseWishartFast(νleft, Sleft), right = InverseWishartFast(νright, Sright)
+                @test test_generic_simple_exponentialfamily_product(
+                    left,
+                    right,
+                    strategies = (PreserveTypeProd(ExponentialFamilyDistribution{InverseWishartFast}), GenericProd())
+                )
+            end
+        end
+    end
 end
 
 end
