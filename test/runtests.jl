253982ba16b63c8ce98f50811751f511d884e2fd
Bagaev Dmitry <bvdmitri@gmail.com>
2023-10-31 09:44:05 +0100

    fix tests on apple chips

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,11 @@ using Aqua, CpuId, ReTestItems, ExponentialFamily
 # `piracy = false` - we extend/add some of the methods to the objects defined in the Distributions.jl
 Aqua.test_all(ExponentialFamily, ambiguities = false, piracy = false)
 
+nthreads = max(cputhreads(), 1)
+ncores = max(cpucores(), 1)
+
 runtests(ExponentialFamily,
-    nworkers = cpucores(),
-    nworker_threads = Int(cputhreads() / cpucores()),
+    nworkers = ncores,
+    nworker_threads = Int(nthreads / ncores),
     memory_threshold = 1.0
 )
