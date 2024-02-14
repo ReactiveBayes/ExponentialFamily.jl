c0f8a526b5835a2db2e1ff8a6e111255fa2ddc37
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2024-02-14 15:23:15 +0100

    test: revert piracy

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Aqua, CpuId, ReTestItems, ExponentialFamily
 
 # `ambiguities = false` - there are quite some ambiguities, but these should be normal and should not be encountered under normal circumstances
 # `piracy = false` - we extend/add some of the methods to the objects defined in the Distributions.jl
-Aqua.test_all(ExponentialFamily, ambiguities = false, deps_compat = (; check_extras = false, check_weakdeps = true), piracies = false)
+Aqua.test_all(ExponentialFamily, ambiguities = false, deps_compat = (; check_extras = false, check_weakdeps = true), piracy = false)
 
 nthreads = max(cputhreads(), 1)
 ncores = max(cpucores(), 1)
