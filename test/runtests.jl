37fc8607ff002b091edf3e12bc173fd3c497a7db
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2024-07-04 18:13:36 +0300

    piracy to piracies

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Aqua, CpuId, ReTestItems, ExponentialFamily
 
 # `ambiguities = false` - there are quite some ambiguities, but these should be normal and should not be encountered under normal circumstances
-# `piracy = false` - we extend/add some of the methods to the objects defined in the Distributions.jl
+# `piracies = false` - we extend/add some of the methods to the objects defined in the Distributions.jl
 Aqua.test_all(ExponentialFamily, ambiguities = false, deps_compat = (; check_extras = false, check_weakdeps = true), piracies = false)
 
 nthreads = max(cputhreads(), 1)
