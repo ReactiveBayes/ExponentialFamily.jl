40f08e278855ec6152a623b044ddd0147b5a9732
Wouter Nuijten <wouternuijten@gmail.com>
2025-01-08 13:13:59 +0100

    Update judge benchmarks

--- scripts/benchmark.jl
+++ scripts/benchmark.jl
@@ -10,6 +10,6 @@ if isempty(ARGS)
     export_markdown("./benchmark_logs/last.md", result)
 else
     name = first(ARGS)
-    BenchmarkTools.judge(ExponentialFamily, name; judgekwargs = Dict(:time_tolerance => 0.1, :memory_tolerance => 0.05))
+    result = BenchmarkTools.judge(ExponentialFamily, name; judgekwargs = Dict(:time_tolerance => 0.1, :memory_tolerance => 0.05))
     export_markdown("benchmark_vs_$(name)_result.md", result)
 end
