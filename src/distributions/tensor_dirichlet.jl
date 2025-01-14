b5a43e2b1aa9e25ca4ac1d683fb510f405ac395c
Wouter Nuijten <wouternuijten@gmail.com>
2025-01-14 12:41:50 +0100

    Update docs

--- src/distributions/tensor_dirichlet.jl
+++ src/distributions/tensor_dirichlet.jl
@@ -17,12 +17,12 @@ const ContinuousTensorDistribution = Distribution{ArrayLikeVariate, Continuous}
 """
     TensorDirichlet{T <: Real, N, A <: AbstractArray{T, N}, Ts} <: ContinuousTensorDistribution
 
-A tensor-valued Dirichlet distribution, where `T` is the element type of the tensor `A`. The distribution generalizes the Dirichlet distribution to handle multiple sets of parameters organized in a tensor structure.
+A tensor-valued Dirichlet distribution, where `T` is the element type of the tensor `A`. The distribution generalizes the Dirichlet distribution to handle multiple sets of parameters organized in a tensor structure. This distribution collects multiple independent Dirichlet distributions into a single efficient interface. The Dirichlet counts for the independent Dirichlet distributions are stored along the first dimension of `a`. This distribution can be used as a conjugate prior to a Categorical distribution with mulitple switch cases (such as a discrete state-transition with controls).  
 
 # Fields
 - `a::A`: The tensor parameter of the distribution, where each slice represents parameters of a Dirichlet distribution
-- `α0::Ts`: The sum of parameters along the first dimension
-- `lmnB::Ts`: The log multinomial beta function values for each slice
+- `α0::Ts`: The sum of parameters along the first dimension.
+- `lmnB::Ts`: The log multinomial beta function values for each slice.
 
 The distribution models multiple independent Dirichlet distributions organized in a tensor structure, where each slice `a[:,i,j,...]` represents the parameters of an independent Dirichlet distribution.
 """
