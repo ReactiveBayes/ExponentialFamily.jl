854d84cc5788a44fc9b00381e7d329c4761eb19c
Tim <t.n.nisslbeck@tue.nl>
2025-10-21 15:32:33 +0200

    #41 Use Hwloc instead of CpuId for cross-platform detect of cores/threads.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,15 @@
-using Aqua, CpuId, ReTestItems, ExponentialFamily
+using Aqua, Hwloc, ReTestItems, ExponentialFamily
 
 # `ambiguities = false` - there are quite some ambiguities, but these should be normal and should not be encountered under normal circumstances
 # `piracies = false` - we extend/add some of the methods to the objects defined in the Distributions.jl
 Aqua.test_all(ExponentialFamily, ambiguities = false, deps_compat = (; check_extras = false, check_weakdeps = true), piracies = false)
 
-nthreads = max(cputhreads(), 1)
-ncores = max(cpucores(), 1)
+ncores   = max(Hwloc.num_physical_cores(), 1)
+nthreads = max(Hwloc.num_virtual_cores(), 1)
+threads_per_core = max(Int(floor(nthreads / ncores)), 1)
 
 runtests(ExponentialFamily,
     nworkers = ncores,
-    nworker_threads = Int(nthreads / ncores),
+    nworker_threads = threads_per_core,
     memory_threshold = 1.0
 )
