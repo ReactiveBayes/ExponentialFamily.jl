c77a5f05077b01a5f9d22828f018e183a86e197e
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2026-01-13 15:52:56 +0100

    fix: add @info because without it, hard to search in many sim. running benchs

--- scripts/benchmark.jl
+++ scripts/benchmark.jl
@@ -13,4 +13,5 @@ else
     result = BenchmarkTools.judge(ExponentialFamily, name; judgekwargs = Dict(:time_tolerance => 0.1, :memory_tolerance => 0.05))
     safe_name = replace(name, '/' => '-')
     export_markdown("./benchmark_logs/benchmark_vs_$(safe_name)_result.md", result)
+    @info "Benchmark logs is saved to ./benchmark_logs/benchmark_vs_$(safe_name)_result.md"
 end
